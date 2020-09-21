fe63c53cc1ced16ac09780a223745cc0c961f3cd
psychome004 <anil.stephen66@gmail.com>
2020-09-21 12:17:42 +0530

    Added new-header

--- functions.php
+++ functions.php
@@ -4,3 +4,6 @@
 add_action('wp_enqueue_scripts',function(){
   wp_enqueue_style( 'hfm-css', get_stylesheet_directory_uri().'/assets/css/hfm-twenty-twenty.css', array('sp-core-style'), time() );
 },99);
+
+//Include Files
+include('lib/custom-header/header-functions.php');
---
+++ lib/custom-header/header-template.php
@@ -0,0 +1,16 @@
+<div class="hfm-navbar-wrapper">
+  <?php do_action('sp_logo');?>
+    <nav class="navbar navbar-default side-navigation">
+      <!-- <div class="container-fluid"> -->
+    		<div class="navbar-header">
+    			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" aria-controls="navbar">
+    				<span class="sr-only">Toggle navigation</span>
+    				<span class="icon-bar top-bar "></span>
+    				<span class="icon-bar middle-bar"></span>
+    				<span class="icon-bar bottom-bar"></span>
+    			</button>
+  			</div>
+    		<?php do_action('sp_nav_menu');?>
+      <!-- </div> -->
+    </nav>
+</div>
