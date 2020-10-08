a1f198b9d0a7d96e7395efe5441c08a65dba21a7
psychome004 <anil.stephen66@gmail.com>
2020-10-08 14:09:21 +0530

    Added single-series template

---
+++ single-series.php
@@ -0,0 +1,8 @@
+<?php get_header();?>
+  <div class="container-fluid single-post-template">
+    <div class="header">
+      <div class="vertical-line"></div>
+      <a href="#" class="btn-nav-back">Back to Series</a>
+    </div>
+  </div>
+<?php get_footer();?>
