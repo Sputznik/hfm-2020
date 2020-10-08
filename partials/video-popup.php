7836f0837b406f274bec1d91da980296b691d6c7
psychome004 <anil.stephen66@gmail.com>
2020-10-08 17:05:45 +0530

    Added orbit video-popup template

---
+++ partials/video-popup.php
@@ -0,0 +1,14 @@
+<?php
+  global $youtube;
+  $video_id = $youtube->get_video_id( get_post_meta( get_the_ID(), 'video_url', true ) );
+  $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
+?>
+<div class="video-thumb" style="background-image: url( <?php _e( $image_url );?> );"></div>
+<div class="video-meta">
+  <h4 class="title"><?php the_title();?></h4>
+  <div class="desc"><?php the_content();?></div>
+  <div class="btn-play" data-video="<?php _e( $video_id );?>" data-behaviour="sp-ytube-video">
+    <span class="play-icon"></span>
+    <span>Play</span>
+  </div>
+</div>
