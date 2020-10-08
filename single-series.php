56a223dcc4cf58d27b9068e59ae6bc6bf4ad65a9
psychome004 <anil.stephen66@gmail.com>
2020-10-08 17:34:09 +0530

    Updated single-series template

--- single-series.php
+++ single-series.php
@@ -2,7 +2,8 @@
   <div class="container-fluid single-post-template">
     <div class="header">
       <div class="vertical-line"></div>
-      <a href="#" class="btn-nav-back">Back to Series</a>
+      <a href="javascript:history.back()" class="btn-nav-back">Back to Series</a>
     </div>
+    <?php _e( do_shortcode( '[orbit_query style="video" post_type="videos" posts_per_page="4"]' ) );?>
   </div>
 <?php get_footer();?>
