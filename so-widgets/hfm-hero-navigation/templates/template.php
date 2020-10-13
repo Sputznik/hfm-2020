35627f9d5accff2aab4e65096ad2582762d60000
unknown <samvthom16@gmail.com>
2020-10-13 11:07:21 +0530

    Refactor css for stories section in homepage

--- so-widgets/hfm-hero-navigation/templates/template.php
+++ so-widgets/hfm-hero-navigation/templates/template.php
@@ -1,8 +1,7 @@
 <!-- HFM HERO NAVIGATION -->
 <div class="hfm-hero-navigation">
-  <h6 class="section-title"><?php _e( $instance['title'] );?></h6>
   <div class="fullwidth">
-    <ul class="list-unstyled slider" data-infinite="false" data-behaviour="hero-navigation-slick" data-items="1" >
+    <div class="slider" data-infinite="false" data-behaviour="hero-navigation-slick" data-items="1" >
       <?php foreach ( $instance['tab_items'] as $i => $tab ) :?>
         <?php $image = wp_get_attachment_url( $tab['tab_image'] );?>
         <div class="hero-pane" id="target-<?php echo getUniqueID( $tab );?>">
@@ -10,20 +9,21 @@
           <div class="overlay"></div>
         </div>
       <?php endforeach;?>
-    </ul>
+    </div>
   </div>
-
-  <ul class="hero-nav-tabs list-unstyled">
-    <?php foreach ( $instance['tab_items'] as $i => $tab ) :?>
-      <li class="hero-tab<?php echo $i == 0 ? ' active' : ''; ?>">
-				<a href="#target-<?php echo getUniqueID( $tab );?>" data-behaviour="hfm-hero-navigation">
-					<?php _e( $tab['tab_title'] );?>
-				</a>
-        <span class="tab-content">
-          <?php _e( $tab['tab_content'] );?>
-        </span>
-			</li>
-    <?php endforeach;?>
-  </ul> <!-- NAV TABS end.. -->
-
+	<div class="hfm-hero-content">
+		<h6 class="section-title"><?php _e( $instance['title'] );?></h6>
+	  <ul class="hero-nav-tabs list-unstyled">
+	    <?php foreach ( $instance['tab_items'] as $i => $tab ) :?>
+	      <li class="hero-tab<?php echo $i == 0 ? ' active' : ''; ?>">
+					<a href="#target-<?php echo getUniqueID( $tab );?>" data-behaviour="hfm-hero-navigation">
+						<?php _e( $tab['tab_title'] );?>
+					</a>
+	        <span class="tab-content">
+	          <?php _e( $tab['tab_content'] );?>
+	        </span>
+				</li>
+	    <?php endforeach;?>
+	  </ul> <!-- NAV TABS end.. -->
+	</div>
 </div> <!-- HFM HERO NAVIGATION ends.. -->
