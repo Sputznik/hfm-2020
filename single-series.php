086c06d7e83aabe3ce284b28b5ac6c8f215bd072
psychome004 <anil.stephen66@gmail.com>
2020-10-19 13:52:28 +0530

    Updated single-series template

--- single-series.php
+++ single-series.php
@@ -3,10 +3,12 @@
     <?php if( have_posts() ): while ( have_posts() ): the_post();?>
       <div class="header">
         <div class="vertical-line"></div>
-        <!-- <a href="javascript:history.back()" class="btn-nav-back">Back to Series</a> -->
-        <span class="btn-nav-back"><?php the_title();?></span>
+        <a href="https://orbit1.sitehub.in/hfm2020/our-work/" class="btn-nav-back">Back to Series</a>
       </div>
-      <div class="post-content"><?php the_content();?></div>
+      <div class="post-content">
+        <h6 class="title"><?php the_title();?></h6>
+        <?php the_content();?>
+      </div>
       <?php _e( do_shortcode( '[orbit_query style="video" post_type="videos" posts_per_page="4"]' ) );?>
     <?php endwhile;endif;?>
   </div>
