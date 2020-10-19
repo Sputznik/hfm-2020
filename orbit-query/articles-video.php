4c3ccd1a049adc20b6123551479c18264f21391a
psychome004 <anil.stephen66@gmail.com>
2020-10-19 17:00:32 +0530

    Updated orbit video-popup template

--- orbit-query/articles-video.php
+++ orbit-query/articles-video.php
@@ -1,9 +1,4 @@
-<?php
-  $wrapper_class = 'grid-2';
-  // $posts_count = $atts['posts_per_page'];
-  // if( $posts_count > 4 ){ $wrapper_class = 'grid-2-3'; }
-?>
-<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled articles-video <?php _e( $wrapper_class );?>">
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled articles-video grid-2">
   <?php $i=1; while( $this->query->have_posts() ) : $this->query->the_post();?>
     <li class="orbit-article">
       <?php include( get_stylesheet_directory().'/partials/video-popup.php' ); ?>
