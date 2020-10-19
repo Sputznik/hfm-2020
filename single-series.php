2a604fb87ffe69f8ef7a5de50fb8adfa664fa048
psychome004 <anil.stephen66@gmail.com>
2020-10-19 12:01:11 +0530

    Updated single-series template

--- single-series.php
+++ single-series.php
@@ -1,9 +1,13 @@
 <?php get_header();?>
   <div class="container-fluid single-post-template">
-    <div class="header">
-      <div class="vertical-line"></div>
-      <a href="javascript:history.back()" class="btn-nav-back">Back to Series</a>
-    </div>
-    <?php _e( do_shortcode( '[orbit_query style="video" post_type="videos" posts_per_page="4"]' ) );?>
+    <?php if( have_posts() ): while ( have_posts() ): the_post();?>
+      <div class="header">
+        <div class="vertical-line"></div>
+        <!-- <a href="javascript:history.back()" class="btn-nav-back">Back to Series</a> -->
+        <span class="btn-nav-back"><?php the_title();?></span>
+      </div>
+      <div class="post-content"><?php the_content();?></div>
+      <?php _e( do_shortcode( '[orbit_query style="video" post_type="videos" posts_per_page="4"]' ) );?>
+    <?php endwhile;endif;?>
   </div>
 <?php get_footer();?>
