2f3600d17e9ed93b2898e151233f95dea9a82c87
psychome004 <anil.stephen66@gmail.com>
2020-10-19 17:04:36 +0530

    Added 3 column video-popup template

---
+++ orbit-query/articles-video-grid3.php
@@ -0,0 +1,7 @@
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled articles-video grid-2-3">
+  <?php $i=1; while( $this->query->have_posts() ) : $this->query->the_post();?>
+    <li class="orbit-article">
+      <?php include( get_stylesheet_directory().'/partials/video-popup.php' ); ?>
+    </li>
+  <?php $i++; endwhile;?>
+</ul>
