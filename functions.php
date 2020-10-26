c06e2da391770fe80b8764ee6378ba949494fba1
unknown <samvthom16@gmail.com>
2020-10-26 20:39:37 +0530

    Ad SO css to theme

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 add_action('wp_enqueue_scripts',function(){
   wp_enqueue_style('barlow-google-fonts', 'https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800;900&display=swap', array('sp-core-style'), '1.0.0' );
   wp_enqueue_style( 'hfm-css', get_stylesheet_directory_uri().'/assets/css/hfm-twenty-twenty.css', array('sp-core-style'), time() );
+	wp_enqueue_style( 'extra-css', get_stylesheet_directory_uri().'/assets/css/extra.css', array('sp-core-style', 'hfm-css'), time() );
   wp_enqueue_script( 'hfm-js', get_stylesheet_directory_uri().'/assets/js/hfm-twenty-twenty.js', array( 'jquery' ), time(), true );
   wp_enqueue_script('slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js', array('jquery'), null, true);
   wp_enqueue_script( 'sow-sliding-navigation', get_stylesheet_directory_uri().'/assets/js/sliding-navigation.js', array( 'jquery' ), time(), true );
