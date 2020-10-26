867593d8caca143e9493c03b91081e2d5e237e15
psychome004 <anil.stephen66@gmail.com>
2020-10-26 15:38:19 +0530

    Updated post-common template

--- partials/post-common.php
+++ partials/post-common.php
@@ -1,13 +1,14 @@
 <?php
   $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
   $post_type = get_post_type();
+  $client_name = get_post_meta( get_the_ID() ,'client_name', true );
   $wrapper_class = ( $post_type == 'series' ) ? 'series-wrapper': 'videos-wrapper';
 ?>
 <div class="<?php _e( $wrapper_class );?>" style="background-image: url( <?php _e( $image_url );?> );">
   <div class="overlay"></div>
   <div class="content">
     <?php if( $post_type == 'videos' ):?>
-      <h4>HUMAN FACTOR MEDIA</h4>
+      <h4><?php _e( $client_name );?></h4>
     <?php endif;?>
     <h2 class="title"><?php the_title();?></h2>
     <?php if( $post_type == 'series' ):?>
